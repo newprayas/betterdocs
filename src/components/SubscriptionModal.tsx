266edb5d30ccd779d0919ceedc0a46db2124607c
Prayas <157107022+newprayas@users.noreply.github.com>
2025-12-17 16:16:51 +0600

    redeploy

--- src/components/SubscriptionModal.tsx
+++ src/components/SubscriptionModal.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Fragment, useState, useEffect, useRef } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { createClient } from '@/utils/supabase/client'
