7ccfc0aa0a9cad2a64fecef054fcf4191d2cf778
Prayas <157107022+newprayas@users.noreply.github.com>
2026-04-22 23:41:55 +0600

    New

--- src/components/SubscriptionModal.tsx
+++ src/components/SubscriptionModal.tsx
@@ -142,7 +142,7 @@ export default function SubscriptionModal() {
                       ref={titleRef}
                       tabIndex={-1}
                     >
-                      Hey there!
+                      Hey there !
                     </Dialog.Title>
 
                     <div className="mt-4 space-y-4 text-left">
